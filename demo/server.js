a9eb03a3920bd127420b7c391e211c1b18ef1c09
Thijs van Hoof <thijs.v.h@gmail.com>
2021-12-07 15:38:07 +0100

    Rename demo/demo.js to demo/server.js and fix lib folder serving

--- demo/server.js
+++ demo/server.js
@@ -9,8 +9,8 @@ const app = express();
 const port = 3000;
 
 // Serve static files
-app.use(express.static(path.join(cwd, 'lib')));
 app.use(express.static(path.join(cwd, 'demo', 'public')));
+app.use(express.static(path.join(cwd, 'lib'), { extensions: ['js'] }));
 
 app.listen(port, () => {
   console.log(`Started demo at http://localhost:${port}`);
