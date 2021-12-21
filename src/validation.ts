c4616abd22814eb3c924ce30ee799a1492eba1b3
Thijs van Hoof <thijs.v.h@gmail.com>
2021-12-21 11:52:44 +0100

    Add portal event header validation

--- src/validation.ts
+++ src/validation.ts
@@ -3,7 +3,12 @@ import {
   PortalDefs,
   APIMessage,
   APIArgument,
+  APIArgumentType,
   APIArgumentTypes,
+  PortalEventHeader,
+  PortalIdArg,
+  EventTypeArg,
+  EventTypes,
 } from "./types";
 
 // External Includes
@@ -12,6 +17,34 @@ import {
   isObject,
   isString,
 } from 'lodash';
+
+
+/**
+ * Tests whether the supplied header is in a valid portal event header format.
+ * Throws an error with descriptive message when the validation fails.
+ * @param header the header to verify
+ * @returns the valid portal event header
+ */
+export function testPortalEventHeader(header: Partial<PortalEventHeader>): PortalEventHeader {
+  const validMessage = testAPIMessage(header);
+
+  if (validMessage.Name !== PortalDefs.eventHeaderName)
+    throw new Error(`Portal event header Name property should be "${PortalDefs.eventHeaderName}", got "${validMessage.Name}"`);
+
+  const portalIdArg: APIArgument | undefined = validMessage.Arguments.find(arg => arg.Name === PortalDefs.portalIDArgName);
+  if (!portalIdArg)
+    throw new Error(`Portal event header is missing the "${PortalDefs.portalIDArgName}" argument`);
+
+  const eventTypeArg: APIArgument | undefined = validMessage.Arguments.find(arg => arg.Name === PortalDefs.eventTypeArgName);
+  if (!eventTypeArg)
+    throw new Error(`Portal event header is missing the "${PortalDefs.eventTypeArgName}" argument`);
+
+  testPortalIdArg(portalIdArg);
+  testEventTypeArg(eventTypeArg);
+
+  return validMessage as PortalEventHeader;
+};
+
 
 /**
  * Tests whether the supplied message is in a valid API message format.
@@ -66,3 +99,43 @@ function testAPIArgument(argument: Partial<APIArgument>): APIArgument {
 
   return argument as APIArgument;
 };
+
+
+/**
+ * Tests whether the supplied argument is in a valid portal ID argument format.
+ * Throws an error with descriptive message when the validation fails.
+ * @param argument the argument to verify
+ * @returns the valid portal ID argument
+ */
+function testPortalIdArg(argument: APIArgument): PortalIdArg {
+  if (argument.Type !== APIArgumentType.String)
+    throw new Error(`Portal ID argument Type property should be "${APIArgumentType.String}", got "${argument.Type}"`);
+
+  if (argument.Name !== PortalDefs.portalIDArgName)
+    throw new Error(`Portal ID argument Name property should be "${PortalDefs.portalIDArgName}", got "${argument.Name}"`);
+
+  if (!isString(argument.Value))
+    throw new Error(`Portal ID argument Value property is not a string: ${argument.Value}`);
+
+  return argument as PortalIdArg;
+}
+
+
+/**
+ * Tests whether the supplied argument is in a valid event type argument format.
+ * Throws an error with descriptive message when the validation fails.
+ * @param argument the argument to verify
+ * @returns the valid event type argument
+ */
+function testEventTypeArg(argument: APIArgument): EventTypeArg {
+  if (argument.Type !== APIArgumentType.String)
+    throw new Error(`Event type argument Type property should be "${APIArgumentType.String}", got "${argument.Type}"`);
+
+  if (argument.Name !== PortalDefs.eventTypeArgName)
+    throw new Error(`Event type argument Name property should be "${PortalDefs.eventTypeArgName}", got "${argument.Name}"`);
+
+  if (!isString(argument.Value) || !EventTypes.includes(argument.Value))
+    throw new Error(`Event type argument Value property is invalid: "${argument.Value}"`);
+
+  return argument as EventTypeArg;
+}
