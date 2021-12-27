111f57cd4adaf6aa246f355c69ccb9a61a877508
Thijs van Hoof <thijs.v.h@gmail.com>
2021-12-27 18:58:52 +0100

    Create generic NAPPortalItem base class

---
+++ src/napportalitem.ts
@@ -0,0 +1,44 @@
+// Local Includes
+import { APIMessage } from './types';
+
+
+/**
+ * NAPPortalItem
+ */
+export class NAPPortalItem extends EventTarget {
+
+  private readonly id: string;        ///< This NAPPortalItem's id
+  private readonly name: string;      ///< This NAPPortalItem's name
+  private readonly tr: HTMLElement;   ///< This NAPPortalItem's table row element
+  private readonly td: HTMLElement;   ///< This NAPPortalItem's table cell element
+
+  /**
+   * Constructor
+   */
+  constructor(message: APIMessage) {
+    super();
+    this.id = message.mID;
+    this.name = message.Name;
+
+    // Create label
+    const label = document.createElement('label');
+    label.setAttribute('for', this.id);
+    label.innerHTML = this.name;
+
+    // Create label cell
+    const labelTd = document.createElement('td');
+    labelTd.appendChild(label);
+
+    // Create content cell
+    this.td = document.createElement('td');
+
+    // Create row
+    this.tr = document.createElement('tr');
+    this.tr.appendChild(labelTd);
+    this.tr.appendChild(this.td);
+  }
+
+  get el(): HTMLElement {
+    return this.tr;
+  }
+}
