b6462aa1e61f320ac5da1438a2e77bcfbd0d23e0
Thijs van Hoof <thijs.v.h@gmail.com>
2022-01-31 11:59:33 +0100

    Add array argument types

--- src/types.ts
+++ src/types.ts
@@ -3,14 +3,14 @@
  */
 
 export class PortalDefs {
-  static readonly apiMessageType = "nap::APIMessage";       ///< Type of an API message
-  static readonly eventHeaderName = "portal_event_header";  ///< Name of the portal event header
-  static readonly portalIDArgName = "portal_id";            ///< Name of the argument containing the portal ID in the portal event header
-  static readonly eventTypeArgName = "portal_event_type";   ///< Name of the argument containing the portal event type in the portal event header
-  static readonly itemTypeArgName = "portal_item_type";     ///< Name of the argument containing the portal item type in the portal item message
-  static readonly itemValueArgName = "portal_item_value";   ///< Name of the argument containing the portal item value in the portal item message
-  static readonly itemMinArgName = "portal_item_min";       ///< Name of the argument containing the minimum portal item value in the portal item message
-  static readonly itemMaxArgName = "portal_item_max";       ///< Name of the argument containing the maximum portal item value in the portal item message
+  static readonly apiMessageType    = "nap::APIMessage";      ///< Type of an API message
+  static readonly eventHeaderName   = "portal_event_header";  ///< Name of the portal event header
+  static readonly portalIDArgName   = "portal_id";            ///< Name of the argument containing the portal ID in the portal event header
+  static readonly eventTypeArgName  = "portal_event_type";    ///< Name of the argument containing the portal event type in the portal event header
+  static readonly itemTypeArgName   = "portal_item_type";     ///< Name of the argument containing the portal item type in the portal item message
+  static readonly itemValueArgName  = "portal_item_value";    ///< Name of the argument containing the portal item value in the portal item message
+  static readonly itemMinArgName    = "portal_item_min";      ///< Name of the argument containing the minimum portal item value in the portal item message
+  static readonly itemMaxArgName    = "portal_item_max";      ///< Name of the argument containing the maximum portal item value in the portal item message
 }
 
 
@@ -42,9 +42,19 @@ export interface APIArgumentString extends APIArgument {
   Value: string,
 }
 
+export interface APIArgumentStringArray extends APIArgument {
+  Type: APIArgumentType.StringArray,
+  Value: Array<string>,
+}
+
 export interface APIArgumentBoolean extends APIArgument {
   Type: APIArgumentType.Boolean,
   Value: boolean,
+}
+
+export interface APIArgumentBooleanArray extends APIArgument {
+  Type: APIArgumentType.BooleanArray,
+  Value: Array<boolean>,
 }
 
 export interface APIArgumentNumeric extends APIArgument {
@@ -56,20 +66,39 @@ export interface APIArgumentNumeric extends APIArgument {
   Value: number,
 }
 
+export interface APIArgumentNumericArray extends APIArgument {
+  Type: APIArgumentType.ByteArray |
+        APIArgumentType.IntArray |
+        APIArgumentType.LongArray |
+        APIArgumentType.FloatArray |
+        APIArgumentType.DoubleArray,
+  Value: Array<number>,
+}
+
 export enum APIArgumentType {
-  String = 'nap::APIString',
-  Boolean = 'nap::APIBool',
-  Byte = 'nap::APIByte',
-  Int = 'nap::APIInt',
-  Long = 'nap::APILong',
-  Float = 'nap::APIFloat',
-  Double = 'nap::APIDouble',
+  String        = 'nap::APIString',
+  StringArray   = 'nap::APIStringArray',
+  Boolean       = 'nap::APIBool',
+  BooleanArray  = 'nap::APIBoolArray',
+  Byte          = 'nap::APIByte',
+  ByteArray     = 'nap::APIByteArray',
+  Int           = 'nap::APIInt',
+  IntArray      = 'nap::APIIntArray',
+  Long          = 'nap::APILong',
+  LongArray     = 'nap::APILongArray',
+  Float         = 'nap::APIFloat',
+  FloatArray    = 'nap::APIFloatArray',
+  Double        = 'nap::APIDouble',
+  DoubleArray   = 'nap::APIDoubleArray',
 };
 
 // Array containing all available API argument types, useful for validation
 export const APIArgumentTypes: Array<string> = Object.values(APIArgumentType);
 
-export type APIArgumentValue = string | number | boolean;
+export type APIArgumentValue =
+  string | Array<string> |
+  number | Array<number> |
+  boolean | Array<boolean>;
 
 
 /**
@@ -125,15 +154,15 @@ export const PortalEventTypes: Array<string> = Object.values(PortalEventType);
  */
 
 export enum PortalItemType {
-  Button = 'nap::PortalItemButton',
-  SliderByte = 'nap::PortalItemSliderByte',
-  SliderInt = 'nap::PortalItemSliderInt',
-  SliderLong = 'nap::PortalItemSliderLong',
-  SliderFloat = 'nap::PortalItemSliderFloat',
-  SliderDouble = 'nap::PortalItemSliderDouble',
-  TextArea = 'nap::PortalItemTextArea',
-  TextField = 'nap::PortalItemTextField',
-  Toggle = 'nap::PortalItemToggle',
+  Button        = 'nap::PortalItemButton',
+  SliderByte    = 'nap::PortalItemSliderByte',
+  SliderInt     = 'nap::PortalItemSliderInt',
+  SliderLong    = 'nap::PortalItemSliderLong',
+  SliderFloat   = 'nap::PortalItemSliderFloat',
+  SliderDouble  = 'nap::PortalItemSliderDouble',
+  TextArea      = 'nap::PortalItemTextArea',
+  TextField     = 'nap::PortalItemTextField',
+  Toggle        = 'nap::PortalItemToggle',
 };
 
 // Array containing all available portal item types, useful for validation
